53222a714ced920dc49476fc9200f56b9606bfc0
Qingyuan Yang <qingyuany@crhtc127.hpc.ucar.edu>
2026-01-08 10:40:23 -0700

    Move fun and class definition to py files, ***

---
+++ funs/hm_class.py
@@ -0,0 +1,188 @@
+import xarray as xr
+import pandas as pd
+import glob
+import os
+import math
+
+import numpy as np
+import re
+from joblib import Parallel, delayed
+from pathlib import Path
+import matplotlib.pyplot as plt
+import alphashape
+from itertools import combinations
+from collections import defaultdict, deque
+from concurrent.futures import ThreadPoolExecutor
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+
+from shapely import points, contains
+import random
+
+def meta_one_hot_shot(meta, para_nm):
+    meta = meta.transpose()
+    meta_one_hot = pd.DataFrame(False, index=meta.index, columns=para_nm)
+    for index, row in meta.iterrows():
+        for r in row.values:
+            meta_one_hot.loc[index, para_nm[r]] = True
+
+    return meta_one_hot
+
+class EmulatedDataStorage:
+    """
+    Lightweight container for emulation outputs.
+    No computation logic.
+    """
+    def __init__(self):
+        pass
+
+class HistoryMatching:
+    def __init__(self, working_dir, case_name, ppe_para, threshold_level = 2.0):
+        self.root = Path(working_dir) / case_name
+        self.tf_masks = pd.read_csv(self.root / f'tf_masks_level_{threshold_level}.csv', index_col=0)
+        self.meta = pd.read_csv(self.root / 'meta.csv', index_col = 0)
+        self.p_emu = xr.open_dataset(self.root / 'sampled_parameters.nc').to_dataframe()
+        self.var_nm = list(self.tf_masks.columns)
+        self.para_nm = list(self.p_emu.columns)
+        self.ppe_para = ppe_para
+        self.tf_masks_raw = self.tf_masks
+
+        self.dropped_vars = EmulatedDataStorage()
+        self.n_sample = self.tf_masks.shape[0]
+
+        self.dropped_vars.nooverlap2d = []
+    def drop_by_name(self, var_to_exclude):
+        var_to_drop = []
+        for v in var_to_exclude:
+            var_to_drop.extend([s for s in self.var_nm if s.startswith(v)])
+        
+        self.tf_masks = self.tf_masks.drop(columns = var_to_drop)
+
+        self.var_nm = list(self.tf_masks.columns)
+        self.dropped_vars.by_name = var_to_drop
+
+    def drop_by_n_survive(self, n_survive):
+        survive_summary = self.tf_masks.sum(axis = 0)
+        self.dropped_vars.useless = list(survive_summary[survive_summary == self.n_sample].index)
+        self.dropped_vars.tight   = list(survive_summary[survive_summary < n_survive].index)
+
+        self.tf_masks = self.tf_masks.drop(columns = self.dropped_vars.useless + self.dropped_vars.tight)
+        
+        self.var_nm = list(self.tf_masks.columns)
+
+    def update_meta(self, occurence_threshold = 2):
+        self.meta = self.meta[self.var_nm]
+        self.meta_onehot = meta_one_hot_shot(self.meta, self.para_nm)
+        p_occur_count = self.meta_onehot.sum(axis = 0)
+        self.p_occur_count = p_occur_count
+        
+#        p_sensitive = list(p_occur_count[p_occur_count > occurence_threshold].index)
+#        self.meta_onehot = self.meta_onehot[p_sensitive]
+
+    def hull_for_each(self, shape_alpha = 5):
+        hull_per_var = {}
+        for v in self.var_nm:
+            p_ind = self.meta[v].sort_values().values
+            pts = self.p_emu[self.tf_masks[v]].iloc[:,p_ind]
+            if pts.shape[0] > 5000:
+               pts = pts.sample(5000)
+
+            pts = pts.values
+            
+            hull_per_var[v] = alphashape.alphashape(pts, shape_alpha)
+
+        self.hull_per_var = hull_per_var
+
+    def group_para_climatology(self):
+
+        vars = self.var_nm
+        paras_vars = {}
+        paras_vars_0 = {}    
+        for c in vars: 
+            para_inds = self.meta[c].sort_values().values
+            para_nms = [self.para_nm[ind] for ind in para_inds]
+            paras_vars.setdefault(tuple(para_nms), []).append(c)
+
+
+
+        paras_vars = dict(
+                sorted(paras_vars.items(), key=lambda item: len(item[1]), reverse=True)
+        )
+        
+        self.paras_vars = paras_vars
+        
+        
+        for k, v in paras_vars.items():
+            temp_count = self.tf_masks[v].all(axis = 1).sum()
+            print(f'{k[0]:<40} and {k[1]:<40}: {temp_count:>8}')
+            if temp_count < 10000:
+                paras_vars_0[k] = v
+
+        self.paras_vars_0 = paras_vars_0
+
+        
+        
+    def shuffle_vars(self, n_comb = 2):
+        summary_table = {}
+    
+        for paras, vars in self.paras_vars_0.items():
+            
+            vars_temp = vars 
+            pd_list = []
+            
+            for vars_comb in combinations(vars_temp, n_comb):
+                pd_list.append(list(vars_comb) + [self.tf_masks[list(vars_comb)].all(axis = 1).sum()])
+                
+            pd_list = pd.DataFrame(pd_list, columns = [f"var{i+1}" for i in range(n_comb)] + ["count"])
+            pd_list = pd_list.sort_values(by = "count")
+            
+            
+            summary_table[paras] = pd_list
+
+        print(f'There are {len(self.paras_vars_0)} groups that have no overlapping within own groups')
+        return summary_table
+
+    def drop_no_overlap2d_vars(self, vars_to_drop):
+        self.tf_masks = self.tf_masks.drop(columns = vars_to_drop)
+        self.var_nm = list(self.tf_masks.columns)
+        self.meta = self.meta[self.var_nm]
+        self.meta_onehot = meta_one_hot_shot(self.meta, self.para_nm)
+        self.dropped_vars.nooverlap2d.append(vars_to_drop)
+
+        
+    
+    def visualize(self, para_pair):
+        para_pair = tuple(para_pair)
+        survive_pts = self.p_emu[self.tf_masks[self.paras_vars[para_pair]].all(axis = 1)]
+
+        if survive_pts.shape[0] > 5000:
+            survive_pts = survive_pts.sample(5000)
+
+        return survive_pts
+
+    def build_hulls(self, shape_alpha = 5):
+        grouped_hulls = {}
+
+        for para2, vars in self.paras_vars.items():
+
+            tf_mask = self.tf_masks[vars].all(axis = 1)
+            pts = self.p_emu[tf_mask]
+            if pts.shape[0] > 5000:
+                pts = pts.sample(5000)
+            
+            pts_np = pts[list(para2)].values
+            
+            grouped_hulls[para2] = alphashape.alphashape(pts_np, shape_alpha)
+            
+            
+        self.grouped_hulls = grouped_hulls
+    
+    def rescale_para(self, sampled_para):
+        ppe_para = self.ppe_para
+        return (ppe_para.max() - ppe_para.min()) * sampled_para + ppe_para.min()
+    
+
+    
+
+    
---
+++ funs/sampling_functions.py
@@ -0,0 +1,184 @@
+
+
+
+def sample_from_hull(X, para, h):
+
+    minx, miny, maxx, maxy = h.bounds
+    p1, p2 = para
+
+    X = X[
+        (X[p1] >= minx) & (X[p1] <= maxx) &
+        (X[p2] >= miny) & (X[p2] <= maxy)
+    ]
+
+    if X.empty:
+        return X
+    
+    pts = points(X[list(para)].values)
+    tf = contains(h,pts)
+    
+    return X.loc[tf].reset_index(drop=True)
+
+
+
+def _one_batch(args):
+    para_l, para_nm, grouped_hulls, n_pts, seed = args
+
+    rng = np.random.default_rng(seed)
+
+    
+    X = pd.DataFrame(
+        rng.uniform(0, 1, size=(n_pts, len(para_nm))),
+        columns=para_nm
+    )
+
+    for para in para_l:
+        h = grouped_hulls[para]
+        X = sample_from_hull(X, para, h)
+
+        if X.shape[0] == 0:
+            return None
+
+    return X
+
+
+def sample_from_hulls_n(
+    para_l,
+    para_nm,
+    grouped_hulls,
+    n_pts=100_000,
+    n_threshold=100_000,
+    max_workers=None,
+    sample_threshold = 10**7
+):
+    if max_workers is None:
+        max_workers = os.cpu_count() -1     
+    
+    
+    out = []
+    count = 0
+    n_sampled = 0
+    
+    with ProcessPoolExecutor(max_workers=max_workers) as ex:
+        futures = []
+        MAX_IN_FLIGHT = 2 * max_workers
+
+        while (count < n_threshold) and (n_sampled < sample_threshold):
+            while (count < n_threshold) and (len(futures) < MAX_IN_FLIGHT) and (n_sampled < sample_threshold):
+                seed = np.random.randint(0, 2**32 - 1)# changed
+                
+                futures.append(ex.submit(_one_batch,
+                    (para_l, para_nm, grouped_hulls, n_pts,seed),))
+
+                n_sampled = n_sampled + n_pts
+
+            
+            # harvest finished jobs
+            for fut in as_completed(list(futures)):
+                futures.remove(fut)
+                X = fut.result()
+                out.append(X)
+
+                
+                if X is not None:
+                    count += X.shape[0]
+        
+                
+                
+        if all([x is None for x in out]):
+            print(f'Find nothing from {sample_threshold} pts')
+            return None
+    
+    out = [x for x in out if x is not None]
+    out = pd.concat(out, axis=0).reset_index(drop = True)
+    return out
+
+
+
+
+
+def test_ind_vars(X_prev, X, para_nm, tf_masks, grouped_hulls, para, paras_vars, shape_alpha = 5):
+    print(f'\t \tRunning test to see if {para} could be break down and lead to non-overlapping')
+
+    vars = paras_vars[para]
+    if len(vars) == 1:
+        return None
+    for i in reversed(range(len(vars))):
+        print(f'Try {i} combinations')
+        var_combs = combinations(vars, i)
+        for var_comb in var_combs:
+            print('Try one of the combinations')
+            X_sub = X[tf_masks[list(var_comb)].all(axis = 1)]
+            if X_sub.shape[0] > 5000:
+                X_sub = X_sub.sample(5000)
+            
+            X_sub = X_sub[list(para)].values
+            hull_sub = alphashape.alphashape(X_sub, shape_alpha)
+
+            attempt = sample_from_hull(X_prev, para, hull_sub)
+            
+            if not attempt.empty:
+                print(f'\t \t \t Found the good variable combo')
+                drop_vars = [x for x in vars if x not in list(var_comb)]
+                return list(var_comb), drop_vars, hull_sub
+            else:
+                pass 
+
+        if i -1 == 0:
+            print(f'\t \t \t No variable combinations work')
+            return None
+
+
+
+
+
+
+def orchestrate_test(para_seq, X, tf_masks, para_nm, grouped_hulls, paras_vars, n_pts=10000, n_threshold=10000, sample_threshold = 10**7, max_workers=None):
+    para_l = []
+    para_non_over = []
+    var_drop = {}
+    
+    non_over_count = 0
+    for p_count, p in enumerate(para_seq):
+        print(f'Running {p}, the {p_count}th simulation')
+        para_l.append(p)
+        out = sample_from_hulls_n(para_l, para_nm, grouped_hulls, n_pts=  n_pts, n_threshold = n_threshold, sample_threshold = sample_threshold, max_workers = max_workers)
+        
+        if out is None:
+            print("Find nothing, try to resolve it by breaking the variables into groups")
+            print("First sample out_prev that needs greater sample size, which will take long")
+            out_prev = sample_from_hulls_n(para_l[:-1], para_nm, grouped_hulls, n_pts=  n_pts, n_threshold = n_threshold, max_workers = max_workers, sample_threshold=sample_threshold * 120)
+            print(f'The size of out_prev is {out_prev.shape[0]}')
+            if out_prev is None:
+                print("out_prev issue")
+    
+            check_pt = test_ind_vars(out_prev, X, para_nm, tf_masks, grouped_hulls, p, paras_vars, shape_alpha = 5)
+            if check_pt is None:
+                para_l.remove(p)
+                del grouped_hulls[p]
+                print(f'\t \t \t \t {p} is causing trouble and is skipped')
+                non_over_count = non_over_count + 1
+                para_non_over.append(p)
+                
+                
+            else:
+                print(f'\t \t \t \t Modify the original hull and variable')
+                print(f'\t \t \t \t Drop {check_pt[1]}')
+                grouped_hulls[p] = check_pt[2]
+                paras_vars[p] = check_pt[0]
+                var_drop[p] = check_pt[1]
+                
+        else:
+            print(f'There is overlap for {p}. Proceed to the next parameter pair')
+            
+
+        print("======================================================================")
+
+    
+        # if non_over_count > 5:
+        #     print("Too many non-overlapping, break the loop")
+        #     return para_l, para_non_over
+
+        
+        
+    return para_l, para_non_over, grouped_hulls, paras_vars, var_drop, out_prev
