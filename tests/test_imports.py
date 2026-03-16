a0cb171b5c0466aafc3c8335ddfd1f99f09c5391
Qingyuan Yang <qingyuany@crlogin3.hpc.ucar.edu>
2026-03-16 12:28:28 -0600

    Add test files

---
+++ tests/test_imports.py
@@ -0,0 +1,12 @@
+def test_public_api_imports():
+    from funs import Analysis, HistoryMatching, Prep_Mask_Generation
+
+
+def test_helper_imports():
+    from funs import (
+        biplot,
+        gp_training_application,
+        metric_cal_single,
+        orchestrate_test,
+        sample_from_hulls_n,
+    )
