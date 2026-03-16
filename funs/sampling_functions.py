f7b1e8abb5251c258c12c0b3d0dbababe8293b82
Qingyuan Yang <qingyuany@crlogin3.hpc.ucar.edu>
2026-03-16 10:11:56 -0600

    Correction for sampling functions

--- funs/sampling_functions.py
+++ funs/sampling_functions.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import os
-from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import alphashape
 from shapely import points, contains
