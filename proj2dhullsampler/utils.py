b0836238d77c4db16044530dbefb9445964887c9
Qingyuan Yang <qy2288@columbia.edu>
2026-06-30 10:30:01 -0600

    Delete some unused funs, add more error check for normalization

--- proj2dhullsampler/utils.py
+++ proj2dhullsampler/utils.py
@@ -25,32 +25,46 @@ import joblib
 
 def gp_training_application(X, Y, y_name, X_emu, path, n_sens_p = 2, no_restart = 10):
     y = Y[y_name]
+
+    if y.isna().any():
+        raise ValueError(
+            f"Diagnostic {y_name!r} contains missing values"
+        )
+
+    y_std = y.std()
+
+    if not np.isfinite(y_std) or y_std == 0:
+        raise ValueError(
+            f"Diagnostic {y_name!r} has zero or invalid variance"
+        )
+
     y_norm = (y - y.mean())/y.std()
 
-    if ~(np.isnan(y_norm).all()):
-        sage1d_temp = fit_all_gp_models_1d(y_norm.values, X.values, len1d = 0.4)
-        
-        
-        sel_para_ind = list(sage1d_temp.index[:n_sens_p])
-        
 
-        kernel = C(1.0, (1e-3, 0.8)) * Matern(length_scale=0.001, nu=2.5, length_scale_bounds=(0.1, 3)) + WhiteKernel(noise_level=0.5, noise_level_bounds=(0.01, 0.9))
-        gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer= no_restart, normalize_y=True)
-        
-        gp.fit(X.values[:,sel_para_ind], y_norm.values)
-        
-        joblib.dump(gp, path + "/python_obj/" + y_name + "_gpmodel.pkl")
+    sage1d_temp = fit_all_gp_models_1d(y_norm.values, X.values, len1d = 0.4)
+    
+    
+    sel_para_ind = list(sage1d_temp.index[:n_sens_p])
+    
 
-        y_mean_emu, y_std_emu = gp.predict(X_emu.values[:,sel_para_ind], return_std=True)
-        y_mean_emu = y_mean_emu.reshape(-1, 1)
-        y_std_emu = y_std_emu.reshape(-1, 1)
+    kernel = C(1.0, (1e-3, 0.8)) * Matern(length_scale=0.001, nu=2.5, length_scale_bounds=(0.1, 3)) + WhiteKernel(noise_level=0.5, noise_level_bounds=(0.01, 0.9))
+    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer= no_restart, normalize_y=True)
+    
+    gp.fit(X.values[:,sel_para_ind], y_norm.values)
+    
+    joblib.dump(gp, path + "/python_obj/" + y_name + "_gpmodel.pkl")
 
-        y_mean_std_emu = np.hstack([y_mean_emu, y_std_emu])
-        y_mean_std_emu = pd.DataFrame(y_mean_std_emu, columns =[y_name + "_mean", y_name + "_std"])
-        y_mean_std_emu.to_csv(path + "/y_emu" + "/gp_mean_std_" + y_name + ".csv")
-        
-        
-        return ([y_name, sel_para_ind])
+    y_mean_emu, y_std_emu = gp.predict(X_emu.values[:,sel_para_ind], return_std=True)
+    y_mean_emu = y_mean_emu.reshape(-1, 1)
+    y_std_emu = y_std_emu.reshape(-1, 1)
+
+    y_mean_std_emu = np.hstack([y_mean_emu, y_std_emu])
+    y_mean_std_emu = pd.DataFrame(y_mean_std_emu, columns =[y_name + "_mean", y_name + "_std"])
+    y_mean_std_emu.to_csv(path + "/y_emu" + "/gp_mean_std_" + y_name + ".csv")
+    
+    
+    return ([y_name, sel_para_ind])
+
 
         
 
